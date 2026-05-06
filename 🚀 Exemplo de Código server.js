5783e58f5a22309cf86de6bceb0ca0c688eb0c2a
wendesonsouza <149152783+Wendesonsouza@users.noreply.github.com>
2026-05-06 10:38:19 -0300

    Update and rename 🚀 Exemplo de Código to 🚀 Exemplo de Código server.js

---
+++ 🚀 Exemplo de Código server.js
@@ -0,0 +1,20 @@
+# server.js
+const express = require('express');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+const pratoRoutes = require('./routes/pratos');
+const adminRoutes = require('./routes/admin');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Rotas
+app.use('/auth', authRoutes);
+app.use('/pratos', pratoRoutes);
+app.use('/admin', adminRoutes);
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000 🚀");
+});
+
